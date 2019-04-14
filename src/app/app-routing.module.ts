0a5ed2f3c32fc4e378dab31d6155a70600058b1d
Nick <nickt@hexaware.com>
2019-04-14 18:34:42 +0100

    Updated Code base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TableComponent } from './table/table.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'side-bar', component: SidenavComponent},
+  { path: 'table', component: TableComponent},
+  { path: 'spinner', component: SpinnerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
